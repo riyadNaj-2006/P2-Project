2b6460188818f7dd9d27b97b0269e8e963e27cf4
Ite_Riyad <riyad.najjar.6.7@gmail.com>
2026-06-08 16:37:12 +0300

    Update Vehicle.java

--- Vehicle.java
+++ Vehicle.java
@@ -1,18 +1,24 @@
 package programming2project;
 
-    public abstract class Vehicle {
-    protected String plateNumber;
-    protected String Brand;
-    protected String Model;
-    protected double dailyPrice;
-    protected boolean Available;
+public abstract class Vehicle {
 
-    Vehicle(String PlateNumber, String Brand, String Model, double DailyPrice, boolean Available) {
-        this.plateNumber = PlateNumber;
-        this.Brand = Brand;
-        this.Model = Model;
-        this.dailyPrice = DailyPrice;
-        this.Available = Available;
+    protected String plateNumber;
+    protected String brand;
+    protected String model;
+    protected double dailyPrice;
+    protected boolean available;
+
+    public Vehicle(String plateNumber,
+                   String brand,
+                   String model,
+                   double dailyPrice,
+                   boolean available) {
+
+        this.plateNumber = plateNumber;
+        this.brand = brand;
+        this.model = model;
+        this.dailyPrice = dailyPrice;
+        this.available = available;
     }
 
     public abstract void printInfo();
