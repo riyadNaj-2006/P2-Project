8aa4d541b3cc4bbed30a3eaac686179b9fd3b42c
Ite_Riyad <riyad.najjar.6.7@gmail.com>
2026-06-08 22:10:17 +0300

    Update Contract.java

--- Contract.java
+++ Contract.java
@@ -1,5 +1,3 @@
-package programming2project;
-
 import java.time.LocalDate;
 
 public class Contract {
@@ -13,8 +11,14 @@ public class Contract {
     LocalDate actualReturnDate;
     int numOfLateDays;
 
-    Contract(String ID, Vehicle vehicle, Customer customer, LocalDate startDate, int numOfRentDays, double basePrice,
-            LocalDate shouldReturnDate, LocalDate actualReturnDate, int numOfLateDays) {
+    Contract(String ID, Vehicle vehicle, 
+              Customer customer, 
+              LocalDate startDate, 
+              int numOfRentDays, 
+              double basePrice,
+              LocalDate shouldReturnDate, 
+              LocalDate actualReturnDate,
+              int numOfLateDays) {
         this.ID = ID;
         this.vehicle = vehicle;
         this.customer = customer;
@@ -37,26 +41,53 @@ public class Contract {
     }
 
     public void printEndedContractsInfo() {
-        System.out.println("ID:" + ID + "  |  Start Date:" + startDate + "  |  Should-End Return Date:"
-                + shouldReturnDate + "  |  Actual Return Date:" + actualReturnDate);
-        System.out.println("Number of Rentdays:" + numOfRentDays + "  |  Number of Late Days:" + numOfLateDays);
-        System.out.println("Base Price:" + basePrice + "  |  Fines:" + fines + "  |  Final Cost:" + finalCost());
+      System.out.println(
+          "ID:" + ID
+        + "  |  Start Date:" + startDate
+        + "  |  Should-End Return Date:"
+        + shouldReturnDate 
+        + "  |  Actual Return Date:" 
+        + actualReturnDate);
+      System.out.println(
+          "Number of Rentdays:" + numOfRentDays 
+        + "  |  Number of Late Days:" 
+        + numOfLateDays);
+        System.out.println(
+          "Base Price:" + basePrice
+        + "  |  Fines:" + fines
+        + "  |  Final Cost:" + finalCost());
     }
 
     public void printRunningContractsInfo() {
-        System.out.println("ID:" + ID + "  |  Start Date:" + startDate + "  |  Should-End Return Date:"
-                + shouldReturnDate + "  |  Actual Return Date:-------");
-        System.out.println("Number of Rentdays:" + numOfRentDays + "  |  Number of Late Days:-------");
-        System.out.println("Base Price:" + basePrice + "  |  Fines:-------" + "  |  Final Cost:-------");
+     System.out.println(
+        "ID:" + ID
+      + "  |  Start Date:" + startDate
+      + "  |  Should-End Return Date:"
+      + shouldReturnDate
+      + "  |  Actual Return Date:-------");
+
+     System.out.println(
+        "Number of Rentdays:" + numOfRentDays
+      + "  |  Number of Late Days:-------");
+        
+     System.out.println(
+        "Base Price:" + basePrice 
+      + "  |  Fines:-------"
+      + "  |  Final Cost:-------");
     }
 
     void displayContractInfo() {
 
         System.out.println("Contract ID: " + ID);
-        System.out.println("Vehicle plateNumber:" + vehicle.plateNumber);
-        System.out.println("Customer ID: " + customer.ID);
-        System.out.println("Starting Date: " + startDate);
-        System.out.println("Number of Rent Days: " + numOfRentDays);
-        System.out.println("Base Price : " + basePrice);
+        System.out.println("Vehicle plateNumber:" 
+                           + vehicle.plateNumber);
+        System.out.println("Customer ID: " 
+                           + customer.ID);
+        System.out.println("Starting Date: " 
+                           + startDate);
+        System.out.println("Number of Rent Days: " 
+                           + numOfRentDays);
+        System.out.println("Base Price : " 
+                           + basePrice);
     }
 }
