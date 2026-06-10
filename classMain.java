82db9d41f706186b33dcb159f090aa3e4714e4b7
Ite_Riyad <riyad.najjar.6.7@gmail.com>
2026-06-10 23:46:19 +0300

    Update classMain.java

--- classMain.java
+++ classMain.java
@@ -3,6 +3,51 @@ import java.util.ArrayList;
 
 class Main {
 
+// Contracts Management
+public static void showAllContracts() {
+
+}
+  
+public static void contractsAndVehicles() {
+
+}
+  
+public static void contractsAndCustomers() {
+
+}
+  
+public static boolean choiceContractsManagementMenu() {
+  int choice = SafeInput.readIntRange("Enter the number of the service which you want: ", 1, 4);
+ 
+  switch(choice) {
+    case 1: 
+      showAllContracts();
+      return true;
+    case 2: 
+      contractsAndVehicles();
+      return true;
+    case 3: 
+      contractsAndCustomers();       
+      return true;
+    default:
+      return true;
+}
+  
+public static void contractsManagementMenu() {
+   boolean running = true;
+  while(running){
+   System.out.println("\"Contracts Management Section\"");
+   System.out.println(
+        "1- Show all contracts" 
+      + "\n2- Contracts & Vehicles"
+      + "\n3- Contracts & Customers"
+    );
+   running = choiceContractsManagementMenu();
+   }
+
+}
+
+
 // Customers Management
 public static void addCustomer() {
 
